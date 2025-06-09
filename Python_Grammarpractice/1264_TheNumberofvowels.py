3339ef16909f70b65eab6a094faa32d76f6e73d6
jisu217 <164330581+jisu217@users.noreply.github.com>
2025-06-09 18:35:40 +0900

    Add files via upload

---
+++ Python_Grammarpractice/1264_TheNumberofvowels.py
@@ -0,0 +1,15 @@
+count = 0
+
+while True :
+    Sentence = input()
+
+    if Sentence == "#" :
+       break
+
+    count = 0
+
+    for char in Sentence.lower() :
+        if char in "aeiou" :
+           count += 1
+
+    print(count)
---
+++ Python_Grammarpractice/4470_LineNumber.py
@@ -0,0 +1,5 @@
+N = int(input()) # 줄의 수
+
+for i in range(1, N+1):
+    Contents = input()  # 각 줄의 내용
+    print(f"{i}. {Contents}")
