df1f66af983072fb37b2d8b60463a18fa7129e94
jisu217 <ji_su217_@naver.com>
2026-03-05 17:10:01 +0900

    Add files via upload

---
+++ programmers/코딩테스트 고득점 Kit/스택-큐/Level2_올바른 괄호/올바른 괄호.py
@@ -0,0 +1,17 @@
+def solution(s):
+    count = 0
+
+    for ch in s:
+        if ch == "(":
+           count += 1
+        else:
+           count -= 1
+
+        if count < 0:
+            return False
+
+    # if count == 0:
+    #    return True
+    # else:
+    #    return False
+    return count == 0
